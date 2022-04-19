7744984849a1fa2d78fd861be4e3a109903d32b8
Tanmay_Pillai <omtej4499@gmail.com>
2022-04-19 13:41:54 +0530

    Uploading Files

---
+++ src/App.js
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Students from './components/Students';
+import Contact from './components/Contact';
+
+const arr = [
+  {
+    name: 'John',
+    age: 26,
+    course: 'MERN',
+    batch: 'October'
+  },
+  {
+    name: 'Doe',
+    age: 25,
+    course: 'MERN',
+    batch: 'November'
+  },
+  {
+    name: 'Biden',
+    age: 26,
+    course: 'MERN',
+    batch: 'September'
+  },
+  {
+    name: 'Barar',
+    age: 22,
+    course: 'MERN',
+    batch: 'September'
+  },
+  {
+    name: 'Christ',
+    age: 23,
+    course: 'MERN',
+    batch: 'October'
+  },
+  {
+    name: 'Elent',
+    age: 24,
+    course: 'MERN',
+    batch: 'November'
+  },
+  {
+    name: 'Tanmay',
+    age: 23,
+    course: 'MERN',
+    batch: 'December'
+  }
+
+];
+
+const App = () => {
+
+  const [students, setStudents] = useState(arr);
+
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/home' element={<Home />}/>
+        <Route path='/students' element={<Students students={students}/>}/>
+        <Route path='contact-us' element={<Contact />}/>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
